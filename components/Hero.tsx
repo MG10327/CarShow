747bbcf7b11bfbf69a96d4c7847abc90e7b122fd
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-19 18:26:53 -0500

    further hero updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,41 +1,45 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
-import {CustomButton} from '@/components'
+import Image from "next/image";
 
+import { CustomButton } from "@components";
 
 const Hero = () => {
-
   const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-  }
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
-    <div>
-      <div className='flex-1 pt-36 padding-x'>
-        <h1 className='hero__title'>Find, book, or rent a car — quickly and easily.</h1>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
 
-        <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
-
-      <CustomButton
-        title="Explore Cars"
-        containerStyles="bg-primary-white rounded-full mt-10"
-        handleClick={handleScroll}
-      />
-
       <div className="hero__image-container">
-        <div className="her__image">
-          <Image src="/../public/hero.png" alt="hero" fill className="object-contain" />
-
-          <div className="hero__image-overlay">
-
-          </div>
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
