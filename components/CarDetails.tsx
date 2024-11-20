bfad3141ab120ed9e325c8e3277d1a3b2e17b5ec
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-20 17:18:36 -0500

    begin modal setup

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { CarProps } from '@types';
+import Image from 'next/image';
+import { Dialog, Transition } from '@headlessui/react';
 
 interface carDetailsProps {
   isOpen: boolean;
@@ -8,9 +10,46 @@ interface carDetailsProps {
 }
 
 
-const CarDetails = () => {
+const CarDetails = ({isOpen, closeModal, car}): carDetailsProps => {
   return (
-    <div>CarDetails</div>
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal} >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className='fixed inset-0 bg-black bg-opacity-25'/>
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+                  <button
+                    type="button"
+                    className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
+                    onClick={closeModal}
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className='object-contain'
+                    />
+
+                  </button>
+                </Dialog.Panel>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   )
 }
 
