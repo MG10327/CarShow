a6d9a9e981366d8cd610cc864a5c7700a076adac
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-19 21:44:54 -0500

    start search setup

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
+import SearchManufacturer from './SearchManufacturer'
+import { manufacturers } from '@constants'
 
 const Searchbar = () => {
+  const [manufacturer, setManufacturer] = useState('')
+
+
+  const handleSearch = () => {
+
+  }
+
   return (
-    <div>Searchbar</div>
+    <form className='search-bar' onSubmit={handleSearch}>
+
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
   )
 }
 
