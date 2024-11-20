51072dd711ee13d4d3d9e0d34f787648ba807186
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-20 18:41:44 -0500

    start custum filters setup

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { Listbox, Transition } from '@headlessui/react'
 
-const CustomFilter = () => {
+const CustomFilter = ({title, options}) => {
   return (
-    <div>CustomFilter</div>
+    <div>
+
+    </div>
   )
 }
 
