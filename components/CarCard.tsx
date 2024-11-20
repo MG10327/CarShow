2658490681e1a597fab517c16d0d5ea83d2217c2
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-19 23:18:21 -0500

    add car rent calculator function

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
+import { CarProps } from '@types';
+import { CustomButton } from '@components';
 
-const CarCard = () => {
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({car}: CarCardProps) => {
+  const {city_mpg, year, make, model, transmission, drive } = car
+
+  const carRent = calculateCarRent(city_mpg, year)
+
   return (
-    <div>CarCard</div>
+    <div className='car-card group'>
+      <div className="car-card__content">
+        <div className='car-card__content'>
+          <h2>{make} {model}</h2>
+        </div>
+
+        <p>
+          <span>
+            {carRent}
+          </span>
+        </p>
+      </div>
+    </div>
   )
 }
 
