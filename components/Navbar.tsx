24100c1e36f055c44cb17104d01638990e1baf53
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-20 21:08:32 -0500

    make button self closing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,9 +20,7 @@ const Navbar = () => {
           title="Sign In"
           btnType='button'
           containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-        >
-
-        </CustomButton>
+        />
       </nav>
 
     </header>
